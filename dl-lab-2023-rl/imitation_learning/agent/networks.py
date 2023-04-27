65356c3b4174a00caf6f77b3c4378ea628524f0a
Sohambasu07 <soham.basu07@gmail.com>
2023-04-28 01:07:25 +0200

    Changed CNN

--- dl-lab-2023-rl/imitation_learning/agent/networks.py
+++ dl-lab-2023-rl/imitation_learning/agent/networks.py
@@ -12,19 +12,19 @@ class CNN(nn.Module):
     def __init__(self, history_length=1, n_classes=5): 
         super(CNN, self).__init__()
         # TODO : define layers of a convolutional neural network
-        self.block1_conv1 = nn.Conv2d(in_channels=history_length, out_channels=16, kernel_size=3, stride=1, padding='same')
-        self.block1_bn1 = nn.BatchNorm2d(num_features=16)
+        self.block1_conv1 = nn.Conv2d(in_channels=history_length, out_channels=32, kernel_size=3, stride=1, padding='same')
+        self.block1_bn1 = nn.BatchNorm2d(num_features=32)
         self.block1_act1 = nn.LeakyReLU(negative_slope=0.2)
-        self.block1_conv2 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3, stride=1, padding='same')
-        self.block1_bn2 = nn.BatchNorm2d(num_features=16)
+        self.block1_conv2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding='same')
+        self.block1_bn2 = nn.BatchNorm2d(num_features=32)
         self.block1_act2 = nn.LeakyReLU(negative_slope=0.2)
         self.block1_pool = nn.MaxPool2d(kernel_size=(2))
         
-        self.block2_conv1 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding='same')
-        self.block2_bn1 = nn.BatchNorm2d(num_features=32)
+        self.block2_conv1 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding='same')
+        self.block2_bn1 = nn.BatchNorm2d(num_features=64)
         self.block2_act1 = nn.LeakyReLU(negative_slope=0.2)
-        self.block2_conv2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, stride=1, padding='same')
-        self.block2_bn2 = nn.BatchNorm2d(num_features=32)
+        self.block2_conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding='same')
+        self.block2_bn2 = nn.BatchNorm2d(num_features=64)
         self.block2_act2 = nn.LeakyReLU(negative_slope=0.2)
         self.block2_pool = nn.AvgPool2d(kernel_size=(24)) #nn.MaxPool2d(kernel_size=(2))
 
@@ -42,23 +42,23 @@ class CNN(nn.Module):
             # self.block3_conv1, self.block3_bn1, self.block3_act1, self.block3_conv2, self.block3_bn2, self.block3_act2, self.block3_pool, 
         ]
 
-        self.conv1 = nn.Conv2d(in_channels=history_length, out_channels=32, kernel_size=3, stride=1, padding='same')
-        self.bn1 = nn.BatchNorm2d(num_features=32)
-        self.act1 = nn.LeakyReLU(negative_slope=0.2)
-        self.pool1 = nn.MaxPool2d(kernel_size=(2))
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding='same')
-        self.bn2 = nn.BatchNorm2d(num_features=64)
-        self.act2 = nn.LeakyReLU(negative_slope=0.2)
-        self.pool2 = nn.AvgPool2d(kernel_size=(48))
+        # self.conv1 = nn.Conv2d(in_channels=history_length, out_channels=32, kernel_size=3, stride=1, padding='same')
+        # self.bn1 = nn.BatchNorm2d(num_features=32)
+        # self.act1 = nn.LeakyReLU(negative_slope=0.2)
+        # self.pool1 = nn.MaxPool2d(kernel_size=(2))
+        # self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding='same')
+        # self.bn2 = nn.BatchNorm2d(num_features=64)
+        # self.act2 = nn.LeakyReLU(negative_slope=0.2)
+        # self.pool2 = nn.AvgPool2d(kernel_size=(48))
 
         self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(64, 32)
-        self.act_fc1 = nn.LeakyReLU(negative_slope=0.02)
+        self.fc1 = nn.Linear(256, 32)
+        self.act_fc1 = nn.LeakyReLU(negative_slope=0.2)
         self.drop1 = nn.Dropout(0.1)
-        self.fc2 = nn.Linear(32, 128)
-        self.act_fc2 = nn.LeakyReLU(negative_slope=0.02)
+        self.fc2 = nn.Linear(32, 512)
+        self.act_fc2 = nn.LeakyReLU(negative_slope=0.2)
         self.drop2 = nn.Dropout(0.5)
-        self.fc_out = nn.Linear(128, n_classes)
+        self.fc_out = nn.Linear(512, n_classes)
         self.output = nn.Softmax(dim=1)
         
 
@@ -66,28 +66,28 @@ class CNN(nn.Module):
 
     def forward(self, x):
         # TODO: compute forward pass
-        # inp = x
-        # blocks = []
-        # count = 0
-        # for layer in self.conv_layers:
-        #     x = layer(x)
-        #     count += 1
-        #     if count == 8: # or count == 15:
-        #         inp = x
-        #     if count == 4 or count == 11: # or count == 18:
-        #     #    blocks.append(x)
-        #     #    count = 0
-        #        x = torch.add(x, inp)
-        #     #    blocks = []
+        inp = x
+        blocks = []
+        count = 0
+        for layer in self.conv_layers:
+            x = layer(x)
+            count += 1
+            if count == 8: # or count == 15:
+                inp = x
+            if count == 4 or count == 11: # or count == 18:
+            #    blocks.append(x)
+            #    count = 0
+               x = torch.add(x, inp)
+            #    blocks = []
 
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.act1(x)
-        x = self.pool1(x)
-        x = self.conv2(x)
-        x = self.bn2(x)
-        x = self.act2(x)
-        x = self.pool2(x)
+        # x = self.conv1(x)
+        # x = self.bn1(x)
+        # x = self.act1(x)
+        # x = self.pool1(x)
+        # x = self.conv2(x)
+        # x = self.bn2(x)
+        # x = self.act2(x)
+        # x = self.pool2(x)
 
 
         x = self.flatten(x)
